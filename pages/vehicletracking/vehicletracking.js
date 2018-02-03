3d151b7805df8c773ed04d685dd4afff17cf6d01
osswangxining <osswangxining@163.com>
2018-02-04 02:17:56 +0800

    png

--- pages/vehicletracking/vehicletracking.js
+++ pages/vehicletracking/vehicletracking.js
@@ -5,7 +5,28 @@ var app = getApp()
 Page({
   data: {
     //默认未获取地址
-    hasLocation: false
+    hasLocation: false,
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      latitude: 23.099994,
+      longitude: 113.324520,
+      title: '位置',
+      iconPath:'/images/location.png',
+      label: {
+        content:'车辆跟踪',
+        color:'#E64340'
+      }
+    }],
+    covers: [{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '/images/location.png'
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '/images/location.png'
+    }]
   },
 
   /*//获取经纬度
@@ -43,13 +64,13 @@ Page({
     if (this.loaded) return;
 
     console.log('x:' + options['x'] + ',y:' + options['y'])
-    if(options['x'] >0 && options['y'] > 0) {
-      that.setData({
-        hasLocation: true,
-        longitude: options['x'],
-        latitude: options['y']
-      })
-    }
+    // if(options['x'] >0 && options['y'] > 0) {
+    //   that.setData({
+    //     hasLocation: true,
+    //     longitude: 23.099994,//options['x'],
+    //     latitude: 113.324520//options['y']
+    //   })
+    // }
   }
 
 })
