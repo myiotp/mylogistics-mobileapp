1477486c2af2879474a605e98e68335696a2c227
osswangxining <osswangxining@163.com>
2018-04-22 19:45:55 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //app.js
 App({
   onLaunch: function () {
+    
+
     this.serviceurl ='https://tongdagufen.cn/onemap';
     this.wsserver='wss://tongdagufen.cn/wsapp/';
     //this.wsserver = 'ws://localhost:8080/onemap/myHandler';
@@ -158,7 +160,35 @@ App({
   },
   onShow: function () {
     console.log("App生命周期函数——onShow函数");
-    
+    /**
+     * 微信强制更新版本机制
+     */
+    const updateManager = wx.getUpdateManager();
+    // 请求完新版本信息的回调
+    updateManager.onCheckForUpdate(function (res) {
+      let version = res.hasUpdate?'有新版本待更新':'最新版本';
+      console.log(version);
+    })
+    //新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      });
+    })
+    // 新的版本下载失败
+    updateManager.onUpdateFailed(function () {
+      wx.showModal({
+        title: '新的版本下载失败,请重新安装白日梦小程序',
+        icon: 'success',
+        duration: 2000
+      });
+    })
   },
   onHide: function () {
     console.log("App生命周期函数——onHide函数");
