9dbb8c93619f1fdc4440f37be5f11bd73cb413a2
osswangxining <osswangxining@163.com>
2018-04-22 16:57:16 +0800

    update

--- pages/trucks4tx/trucks.js
+++ pages/trucks4tx/trucks.js
@@ -11,20 +11,20 @@ Page({
 		that.data.cid = options['cid'];
 		that.data['cargoowner'] = options['o'];
 		console.log(that.data)
-		wx.request({
-      		url: app.serviceurl + '/api/truck/username/'+app.uid + '/status/todo',
-			data:{
+		// wx.request({
+      	// 	url: app.serviceurl + '/api/truck/username/'+app.uid + '/status/todo',
+		// 	data:{
 
-			},
-			success:function(res){
-				that.setData({
-					list:res.data.data,
-					size:res.data.size,
-					cid:options['cid'],
-					cargoowner:options['o']
-				})
-			}
-		})
+		// 	},
+		// 	success:function(res){
+		// 		that.setData({
+		// 			list:res.data.data,
+		// 			size:res.data.size,
+		// 			cid:options['cid'],
+		// 			cargoowner:options['o']
+		// 		})
+		// 	}
+		// })
 	},
 	onShow:function(options){
 		var that = this;
