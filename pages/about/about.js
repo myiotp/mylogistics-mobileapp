74cd8078e520298d74620d427959b434e21ec835
osswangxining <osswangxining@163.com>
2018-04-22 14:47:27 +0800

    update

--- pages/about/about.js
+++ pages/about/about.js
@@ -3,10 +3,28 @@
 var app = getApp();
 Page({
   data: {
-    showLog: false
+    showLog: false,
+    content:''
   },
   onLoad: function(){
+    var that = this;
+    wx.request({
+      url: app.serviceurl + '/api/wx/about',
+      data:{
 
+      },
+      success:function(res){
+        console.log(res.data)
+        if(res.data.data == null || res.data.data == '') {
+          
+        } else {
+          that.setData({
+            content:res.data.data
+          })
+        }
+        
+      }
+    })
   },
   toggleLog: function(){
     this.setData({
